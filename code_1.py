b963682b932c03d3bade0ea80535954b8b7d34db
Javier Arroyo Rojas <jpereza2@ucenfotec.ac.cr>
2023-11-15 23:07:27 -0600

    Update0

--- code_1.py
+++ code_1.py
@@ -1,35 +1,44 @@
+import time
 import board
 import digitalio
-from time import sleep
-import hcsr04
-#from hcsr04 import HCSRO4
+import pulseio
 
-# Configura el pin Trig y el pin Echo del sensor HC-SR05
-buzzer = digitalio.DigitalInOut(board.IO33)
-trig = board.IO33
-echo = board.IO27
+TRIG_PIN = board.D13
+ECHO_PIN = board.D12
+PIEZO_PIN = board.D11
 
-# Crea un objeto HC-SR05
-sonar = hcsr04.HCSR04(trig, echo)
+trig = digitalio.DigitalInOut(TRIG_PIN)
+trig.direction = digitalio.Direction.OUTPUT
+
+echo = digitalio.DigitalInOut(ECHO_PIN)
+echo.direction = digitalio.Direction.INPUT
+
+piezo = pulseio.PWMOut(PIEZO_PIN, duty_cycle=0, frequency=440, variable_frequency=True)
 
 while True:
-    dist = sonar.dist_cm()
-    print(dist)
-    if dist >= 50 or dist <= 0:
-        buzzer.value = False;
-    elif dist >= 30:
-        buzzer.value = True;
-        sleep(0.45)
-        buzzer.value = False;
-    elif dist >= 20:
-        buzzer.value = True;
-        sleep(0.35)
-        buzzer.value = False;
-    elif dist >= 10:
-        buzzer.value = True;
-        sleep(0.25)
-        buzzer.value = False;
-    elif dist >= 5:
-        buzzer.value = True;
-        sleep(0.15)
-        buzzer.value = False;
+    duration, distance = 0, 0
+    trig.value = False
+    time.sleep(0.000002)
+    trig.value = True
+    time.sleep(0.00001)
+    trig.value = False
+    duration = pulseio.pulse_in(echo, True, 1000000)
+    distance = duration / 29 / 2
+    print("Distancia: ", distance, " cm")
+    if distance >= 50 or distance <= 0:
+        piezo.duty_cycle = 0
+    elif distance >= 30:
+        piezo.duty_cycle = 65535 // 2
+        time.sleep(0.45)
+        piezo.duty_cycle = 0
+    elif distance >= 20:
+        piezo.duty_cycle = 65535 // 2
+        time.sleep(0.15)
+        piezo.duty_cycle = 0
+    elif distance >= 10:
+        piezo.duty_cycle = 65535 // 2
+        time.sleep(0.05)
+        piezo.duty_cycle = 0
+    else:
+        piezo.duty_cycle = 65535 // 2
+        time.sleep(0.15)
